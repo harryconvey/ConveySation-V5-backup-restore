213dea7396a5a0d3aa7eebffc3d68491d1b1be18
Harry Convey <harry.convey@gmail.com>
2013-05-18 20:18:55 +0100

    Added Colours

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class UserTest < ActiveSupport::TestCase
   test "a user should enter a first name" do 
   	user = User.new
@@ -7,7 +6,6 @@ class UserTest < ActiveSupport::TestCase
   	assert !user.errors[:first_name].empty?
   
   end
-
 test "a user should enter a last name" do 
   	user = User.new
   	assert !user.save
@@ -21,5 +19,17 @@ test "a user should enter a profile name" do
   	assert !user.errors[:profile_name].empty?
   
   end
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = 'harryconvey'
+    user.first_name = 'Harry'
+    user.email= 'harryconvey.convey@gmail.com'
+    user.last_name = 'Convey'
+    user.password = 'harry1999'
+    user.password_confirmation = 'harry1999'
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
 
+  end
 end
